4715c5b2f432d77cc4f41481469d0d94c539918e
Zijian <Shaddoll@users.noreply.github.com>
2024-07-27 22:23:57 +0000

    more logs

--- service/matching/tasklist/task_list_manager.go
+++ service/matching/tasklist/task_list_manager.go
@@ -560,6 +560,7 @@ func (c *taskListManagerImpl) newChildContext(
 	remaining := time.Until(deadline) - tailroom
 	if remaining < timeout {
 		timeout = time.Duration(common.MaxInt64(0, int64(remaining)))
+		c.logger.Error("remaining", tag.Number(timeout.Milliseconds()), tag.WorkflowPollContextTimeout(timeout))
 	}
 	return context.WithTimeout(parent, timeout)
 }
